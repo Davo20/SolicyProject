c5575ece8e3b5d7b1a9bba39eaaa15d962543578
Davo20 <dav507805@gmail.com>
2022-08-06 17:32:42 +0300

    SolicyProject

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer(){
+    return(
+        <footer>
+            <span>The given project performed David Balabekyan</span>
+        </footer>
+    )
+}
